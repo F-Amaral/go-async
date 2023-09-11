2ecc5b992accbd315cdbc30559e8dc7a7fd7c477
Felipe Amaral <felipemelloamaral@gmail.com>
2023-09-11 20:34:04 -0300

    add on fail tests

--- pool/jobpool_test.go
+++ pool/jobpool_test.go
@@ -25,4 +25,22 @@ func TestJobPool_Process(t *testing.T) {
 		assert.Equal(t, len(dataSet), len(res.Results))
 		assert.False(t, res.HasError)
 	})
+	t.Run("should process all inputs and return error", func(t *testing.T) {
+		// arrange
+		dataSet := make([]int, 100)
+		for i := 0; i < 100; i++ {
+			dataSet[i] = i
+		}
+
+		sut := pool.NewPool[int, int](10, func(i int) (int, error) {
+			return i, assert.AnError
+		})
+
+		// act
+		res := sut.Process(dataSet)
+
+		// assert
+		assert.Equal(t, len(dataSet), len(res.Results))
+		assert.True(t, res.HasError)
+	})
 }
