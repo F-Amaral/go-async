81f36a6531770a58d62165de5f5fa1ecba0373be
Felipe Amaral <felipemelloamaral@gmail.com>
2023-09-20 12:09:22 -0300

    Feature/reduce allocs (#2)
    * simplify ProcessResult GetError
    
    * fix lint issues

--- async/processor.go
+++ async/processor.go
@@ -19,14 +19,6 @@ type ProcessResult struct {
 	HasError bool
 }
 
-// GetError return bool when job has failed and the error
-func (j *JobResult) GetError() (bool, error) {
-	if j.Err != nil {
-		return true, j.Err
-	}
-	return false, nil
-}
-
 // GetErrors return all errors from failed jobs, it uses HasError flag to minimize allocations on getting errors
 func (p *ProcessResult) GetErrors() (errs []error) {
 	if p.HasError {
